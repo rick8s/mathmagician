d8ac446383be3b7d102b249415ef6111b590d1dd
Rick8s <rick8s@hotmail.com>
2015-10-23 14:31:17 -0500

    completed natural numbers implementation with testing

---
+++ Mathmagician/NaturalNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//added "public" to line 10 here to resolve build errors in NaturalNumbersTest
+namespace Mathmagician
+{
+    public class NaturalNumbers
+    {
+        private int first; //create data member with next line
+
+        public NaturalNumbers() //define a constuctor
+        {
+            first = 1;
+        }
+
+        public int GetNext(int x)
+        {
+            return x + 1;
+        }
+
+        public int GetFirst()
+        {
+            return first;
+        }
+
+        public int[] GetSequence()
+        {
+
+            return GetSequence(10);
+        }
+
+        public int[] GetSequence(int length)
+        {
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+            return output.ToArray();
+        }
+
+        public string ToString(int[] array)
+        {
+            string output = String.Join(" ", array);
+            {
+                return output;
+            }
+        }
+    }
+}
